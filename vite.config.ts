9b169d94e20677741dffb0a8ec0a08cbddeb8e25
KUmarRahul001 <kumarrahulraj468@outlook.com>
2025-11-09 01:48:24 +0530

    Automated sitemap generation with vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://nextorra.netlify.app',
+      changefreq: 'monthly',
+      priority: 0.8,
+      dynamicRoutes: [
+        '/',                         // homepage
+        '/get-started',
+        '/internship',
+        '/services',
+        '/services/social-media-marketing',
+        '/services/lead-generation',
+        '/services/sms-marketing',
+        '/services/website-design',
+        '/services/full-stack-web-app',
+        '/services/email-marketing',
+        '/services/missed-call-service',
+        '/services/graphic-design',
+        '/services/voice-call-services',
+        '/services/app-development',
+        '/privacy-policy',
+        '/terms-and-conditions'
+      ],
+    })
+  ],
   build: {
     outDir: 'dist',
   },
